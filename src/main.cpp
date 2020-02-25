d8c5bc28a090d27606ea8ebe7efb88e77b7b1363
Stephan Hasenkamp <sh@synstream.de>
2020-02-25 17:23:19 +0100

    json movement

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,33 @@ const int dns_port = 53;
 const int http_port = 80;
 const int ws_port = 1337;
 // const int led_pin = 15;
-const int led_pin = 2;//ttgo 
+const int led_pin = 2; // ttgo
 // Globals
 AsyncWebServer server(80);
 IPAddress IP;
 WebSocketsServer webSocket = WebSocketsServer(1337);
-char msg_buf[10];
+char msg_buf[2048];
 int led_state = 0;
-uint ts_lastRead = 0;
+uint ts_lastMovementRead = 0;
+uint ts_lastDistanceRead = 0;
+uint ts_lastDHT11Read = 0;
 uint ts = 0;
+
+uint8_t ptrEvent = 0;
+moveEvent_t mvE[D_EVENTCOUNT];
+uint8_t moveState = 0;
+
+DHT_Unified dht(D_DHT11, DHT11);
+
+// Variables to save date and time
+String formattedDate;
+
+WiFiUDP ntpUDP;
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 0);
 /***********************************************************
  * Functions
  */
@@ -76,6 +94,7 @@ void onWebSocketEvent(uint8_t client_num, WStype_t type, uint8_t *payload,
     break;
   }
 }
+
 // Callback: send homepage
 void onIndexRequest(AsyncWebServerRequest *request) {
   IPAddress remote_ip = request->client()->remoteIP();
@@ -105,8 +124,6 @@ void onPageNotFound(AsyncWebServerRequest *request) {
  */
 void setup() {
   // Init LED and turn off
-  pinMode(led_pin, OUTPUT);
-  digitalWrite(led_pin, LOW);
 
   // Start Serial port
   Serial.begin(115200);
@@ -117,59 +134,71 @@ void setup() {
     while (1)
       ;
   }
-/*
-  // Start access point
-  WiFi.softAP(ssid, password);
-  // Print our IP address
+  /*
+    // Start access point
+    WiFi.softAP(ssid, password);
+    // Print our IP address
+    Serial.println();
+    Serial.println("AP running");
+    Serial.print("My IP address: ");
+    Serial.println(WiFi.softAPIP());
+    */
   Serial.println();
-  Serial.println("AP running");
-  Serial.print("My IP address: ");
-  Serial.println(WiFi.softAPIP());
-  */
- Serial.println();
-    Serial.println("connecting to");
-    Serial.println(ssid);
-    uint8_t addr[4];
-    String ipaddr = "192.168.18.10";
-    for (int i = 0; i < 4; i++) {
-      addr[i] = explode2int(ipaddr, '.');
-    }
-    IPAddress ip(addr[0], addr[1], addr[2], addr[3]);
-    ipaddr = "192.168.18.1";
-    for (int i = 0; i < 4; i++) {
-      addr[i] = explode2int(ipaddr, '.');
-    }
-    IPAddress gateway(addr[0], addr[1], addr[2], addr[3]);
-    ipaddr = "255.255.255.0";
-    for (int i = 0; i < 4; i++) {
-      addr[i] = explode2int(ipaddr, '.');
-    }
-    IPAddress subnet(addr[0], addr[1], addr[2], addr[3]);
-    WiFi.config(ip, gateway, subnet);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    uint8_t ccount = 0;
-    Serial.print(ssid);
-    Serial.print("-");
-    Serial.print(password);
-    while (WiFi.status() != WL_CONNECTED && ccount < 20) {
-      delay(1500);
-      ccount++;
-      Serial.print(".");
-    }
-    if (WiFi.status() != WL_CONNECTED) {
-      Serial.print("WiFi NOT connected:");
-      Serial.print(WiFi.status());
-      Serial.print("<");
-    } else {
-      Serial.print("WiFi connected:");
-      Serial.print(WiFi.status());
-      Serial.println(WiFi.localIP());
-      IP = WiFi.localIP();
-      Serial.println(IP);
-      Serial.print("<");
-      //network = true;
-    }
+  Serial.println("connecting to");
+  Serial.println(ssid);
+  uint8_t addr[4];
+  String ipaddr = "192.168.18.10";
+  for (int i = 0; i < 4; i++) {
+    addr[i] = explode2int(ipaddr, '.');
+  }
+  IPAddress ip(addr[0], addr[1], addr[2], addr[3]);
+
+  ipaddr = "192.168.18.1";
+  for (int i = 0; i < 4; i++) {
+    addr[i] = explode2int(ipaddr, '.');
+  }
+  IPAddress gateway(addr[0], addr[1], addr[2], addr[3]);
+
+  ipaddr = "255.255.255.0";
+  for (int i = 0; i < 4; i++) {
+    addr[i] = explode2int(ipaddr, '.');
+  }
+  IPAddress subnet(addr[0], addr[1], addr[2], addr[3]);
+
+  ipaddr = "192.168.18.1";
+  for (int i = 0; i < 4; i++) {
+    addr[i] = explode2int(ipaddr, '.');
+  }
+  IPAddress dns(addr[0], addr[1], addr[2], addr[3]);
+
+  WiFi.config(ip, gateway, subnet, dns);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  uint8_t ccount = 0;
+  Serial.print(ssid);
+  Serial.print("-");
+  Serial.print(password);
+  while (WiFi.status() != WL_CONNECTED && ccount < 20) {
+    delay(1500);
+    ccount++;
+    Serial.print(".");
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("WiFi NOT connected:");
+    Serial.print(WiFi.status());
+    Serial.print("<");
+  } else {
+    Serial.print("WiFi connected:");
+    Serial.println(WiFi.status());
+    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.dnsIP());
+    Serial.println(WiFi.gatewayIP());
+
+    IP = WiFi.localIP();
+    Serial.println(IP);
+    Serial.print("<");
+    // network = true;
+  }
   // On HTTP request for root, provide index.html file
   server.on("/", HTTP_GET, onIndexRequest);
 
@@ -185,24 +214,185 @@ void setup() {
   // Start WebSocket server and assign callback
   webSocket.begin();
   webSocket.onEvent(onWebSocketEvent);
+
+  pinMode(D_SR04_EchoPin, INPUT_PULLDOWN);
+  pinMode(D_SR04_TriggerPin, OUTPUT);
+  pinMode(D_BEWEGuNGSSENSOR, INPUT_PULLDOWN);
+  digitalWrite(D_SR04_TriggerPin, LOW);
+  /*  Serial.printf("trigPin1: %d\n", digitalRead(D_SR04_TriggerPin));
+    delay(5000);
+    digitalWrite(D_SR04_TriggerPin, HIGH);
+    Serial.printf("trigPin2: %d\n", digitalRead(D_SR04_TriggerPin));
+    delay(5000);
+    digitalWrite(D_SR04_TriggerPin, LOW);
+    Serial.printf("trigPin3: %d\n", digitalRead(D_SR04_TriggerPin));*/
+
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // NTP
+  timeClient.begin();
+  for (int i = 0; i < D_EVENTCOUNT; i++) {
+    mvE[i].ts = 0;
+    mvE[i].state = 0;
+  }
 }
 
 void loop() {
   ts = millis();
+
   // Look for and handle WebSocket data
   webSocket.loop();
-  if (ts > ts_lastRead + 3000) {
-    ts_lastRead = millis();
-    led_state = digitalRead(led_pin);
-    Serial.printf("led_state : %d\n", led_state);
-    
-    led_state = led_state ? 0 : 1;
-    digitalWrite( led_pin, led_state);
-    sprintf(msg_buf, "%d", led_state);
-    Serial.printf("Sending loop [%u]: %s\n", 0, msg_buf);
+  if (ts > ts_lastMovementRead + 2000) {
+
+    ts_lastMovementRead = millis();
+    bool movement = digitalRead(D_BEWEGuNGSSENSOR);
+
+    Serial.printf("Move?: %d\n", movement);
+    // webSocket.sendTXT(0, msg_buf);
+    timeClient.update();
+    // formattedDate = (timeClient.getDay() + " " + formattedDate).c_str();
+
+    Serial.println(formattedDate);
+    if (moveState == movement) {
+      ptrEvent++;
+      if (ptrEvent >= D_EVENTCOUNT) {
+        ptrEvent = 0;
+      }
+      timeClient.update();
+      mvE[ptrEvent].state = movement;
+      mvE[ptrEvent].ts = timeClient.getEpochTime();
+      const int capacity = JSON_OBJECT_SIZE(4 * D_EVENTCOUNT + 1);
+      StaticJsonDocument<capacity> jsondoc;
+      jsondoc["type"] = "movement";
+      for (int i = 0; i < D_EVENTCOUNT; i++) {
+       // String varname = "ts_" + String(i);
+        //Serial.println(varname);
+        jsondoc["ts_" + String(i)] = mvE[i].ts;
+        jsondoc["state_" + String(i)] = mvE[i].state;
+      }
+      Serial.println(msg_buf);
+      serializeJson(jsondoc, msg_buf);
+      // Serial.println(msg_buf);
+      webSocket.sendTXT(0, msg_buf);
+      delay(5900);
+    }
+    moveState = movement;
+  }
+  if (ts > ts_lastDistanceRead + 200000) {
+
+    uint distance = getDistance();
+    // Serial.printf("distance: %d\n", distance);
+    ts_lastDistanceRead = millis();
+    timeClient.update();
+    formattedDate = (timeClient.getDay() + " " + formattedDate).c_str();
+    const int capacity = JSON_OBJECT_SIZE(3);
+    StaticJsonDocument<capacity> jsondoc;
+    jsondoc["type"] = "distance";
+    jsondoc["distance"] = distance;
+    jsondoc["ts"] = ts_lastDistanceRead / 1000;
+    serializeJson(jsondoc, msg_buf);
+    // Serial.println(msg_buf);
+    webSocket.sendTXT(0, msg_buf);
+  }
+  if (ts > ts_lastDHT11Read + 5000) {
+    ts_lastDHT11Read = millis();
+    sensors_event_t event;
+
+    timeClient.update();
+    formattedDate = timeClient.getFormattedTime();
+    String s = String(timeClient.getDay()) + " " + formattedDate;
+    Serial.println("s: ");
+    Serial.println(s);
+    Serial.println(s.c_str());
+    const int capacity = JSON_OBJECT_SIZE(4);
+    StaticJsonDocument<capacity> jsondoc;
+    jsondoc["type"] = "klima";
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    } else {
+      Serial.print(F("Temperature: "));
+      Serial.print(event.temperature);
+      Serial.println(F("°C"));
+    }
+    jsondoc["temperatur"] = event.temperature;
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    } else {
+      Serial.print(F("Humidity: "));
+      Serial.print(event.relative_humidity);
+      Serial.println(F("%"));
+    }
+    jsondoc["luftfeuchtigkeit"] = event.relative_humidity;
+    jsondoc["datetime"] =
+        timeClient.getEpochTime(); // s.c_str();//(String(timeClient.getDay()) +
+                                   // " " + formattedDate).c_str();
+
+    serializeJson(jsondoc, msg_buf);
+    // Serial.println(msg_buf);
     webSocket.sendTXT(0, msg_buf);
   }
 }
+
+uint getDistance() {
+  uint entfernung = 0;
+  uint zeit = 0;
+
+  digitalWrite(D_SR04_TriggerPin, LOW);
+  delayMicroseconds(3);
+  noInterrupts();
+  digitalWrite(D_SR04_TriggerPin, HIGH); // Trigger Impuls 10 us
+  delayMicroseconds(10);
+  digitalWrite(D_SR04_TriggerPin, LOW);
+  zeit = pulseIn(D_SR04_EchoPin, HIGH); // Echo-Zeit messen
+  interrupts();
+  zeit = (zeit / 2);        // Zeit halbieren
+  entfernung = zeit / 29.1; // Zeit in Zentimeter umrechnen
+  return (entfernung);
+}
+
 int explode2int(String &s, char d) {
   int index;
   int num;
--- src/main.h
+++ src/main.h
@@ -6,4 +6,20 @@
 #include <WebSocketsServer.h>
 #include <WiFi.h>
 #include <ArduinoJson.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#define D_SR04_EchoPin 14
+#define D_SR04_TriggerPin 12
+#define D_DHT11     13
+#define D_BEWEGuNGSSENSOR 15
+#define D_EVENTCOUNT 20
 int explode2int(String &s, char d);
+uint getDistance();
+
+typedef struct {
+    uint8_t state;
+    uint32_t ts;
+} moveEvent_t;
