135e9458e94b6ea5dd37d2a46bd2cb0f44db37c6
Stephan Hasenkamp <sh@synstream.de>
2020-04-14 22:12:01 +0200

    deepsleep

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ const int http_port = 80;
 const int ws_port = 1337;
 // const int led_pin = 15;
 const int led_pin = 2; // ttgo
+
+RTC_DATA_ATTR static time_t last; // remember last boot in RTC Memory
 // Globals
 AsyncWebServer server(80);
 IPAddress IP;
@@ -20,6 +22,7 @@ int led_state = 0;
 uint ts_lastMovementRead = 0;
 uint ts_lastDistanceRead = 0;
 uint ts_lastDHT11Read = 0;
+uint ts_DeepSleepTime = 60000;
 uint ts = 0;
 
 uint8_t ptrEvent = 0;
@@ -37,6 +40,111 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 0);
 /***********************************************************
  * Functions
  */
+
+/*void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}*/
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+  if (mqttClient.connected()) {
+    Serial.println("Connected MQTT");
+  } else {
+    Serial.println("NOT Connected MQTT...");
+  }
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch (event) {
+  case SYSTEM_EVENT_STA_GOT_IP:
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    connectToMqtt();
+    break;
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    Serial.println("WiFi lost connection");
+    xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+    xTimerStart(wifiReconnectTimer, 0);
+    break;
+  }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  /*
+uint16_t publish(const char* topic, uint8_t qos, bool retain, const char* payload = nullptr, size_t length = 0, bool dup = false, uint16_t message_id = 0)
+  */
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+
+  /*uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+ */
+
+  // mqttClient.publish("keller", 0, true, "test 1");
+  Serial.println("Publishing at QoS 0");
+  // uint16_t packetIdPub1 =
+  mqttClient.publish("keller/temperatur", 1, true, "10");
+
+  /*Serial.print("Publishing at QoS 1, packetId: ");
+  Serial.println(packetIdPub1);
+  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+  Serial.print("Publishing at QoS 2, packetId: ");
+  Serial.println(packetIdPub2);*/
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  // int8_t x = 0;
+  // String sx = reason;
+  // Serial.println(x);
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
 
 // Callback: receiving any WebSocket message
 void onWebSocketEvent(uint8_t client_num, WStype_t type, uint8_t *payload, size_t length) {
@@ -112,30 +220,7 @@ void onPageNotFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-/***********************************************************
- * Main
- */
-void setup() {
-  // Init LED and turn off
-
-  // Start Serial port
-  Serial.begin(115200);
-
-  // Make sure we can read the file system
-  if (!SPIFFS.begin()) {
-    Serial.println("Error mounting SPIFFS");
-    while (1)
-      ;
-  }
-  /*
-    // Start access point
-    WiFi.softAP(ssid, password);
-    // Print our IP address
-    Serial.println();
-    Serial.println("AP running");
-    Serial.print("My IP address: ");
-    Serial.println(WiFi.softAPIP());
-    */
+void connectToWifi() {
   Serial.println();
   Serial.println("connecting to");
   Serial.println(ssid);
@@ -190,24 +275,67 @@ void setup() {
     IP = WiFi.localIP();
     Serial.println(IP);
     Serial.print("<");
-    // network = true;
   }
+}
+/***********************************************************
+ * Main
+ */
+void setup() {
+  // Init LED and turn off
+
+  // Start Serial port
+  Serial.begin(115200);
+  print_wakeup_reason();
+  connectToWifi();
+
+  // Make sure we can read the file system
+  if (!SPIFFS.begin()) {
+    Serial.println("Error mounting SPIFFS");
+    while (1)
+      ;
+  }
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  WiFi.onEvent(WiFiEvent);
+  Serial.print("nach wifievent.......");
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCredentials("domo", "domoomod");
+  Serial.print("nach onmqttt.......");
+  delay(1000);
+  // network = true;
+
   // On HTTP request for root, provide index.html file
   server.on("/", HTTP_GET, onIndexRequest);
+  Serial.print("nach onmqttt2.......");
 
   // On HTTP request for style sheet, provide style.css
   server.on("/style.css", HTTP_GET, onCSSRequest);
+  Serial.print("nach onmqttt.3......");
 
   // Handle requests for pages that do not exist
   server.onNotFound(onPageNotFound);
+  Serial.print("nach onmqttt.......");
 
   // Start web server
   server.begin();
+  Serial.print("webserver begin.......");
 
   // Start WebSocket server and assign callback
   webSocket.begin();
   webSocket.onEvent(onWebSocketEvent);
-
+  Serial.print("nach websocket......");
+  delay(3000);
+  rtc_gpio_deinit((gpio_num_t)D_BEWEGuNGSSENSOR);
   pinMode(D_SR04_EchoPin, INPUT_PULLDOWN);
   pinMode(D_SR04_TriggerPin, OUTPUT);
   pinMode(D_BEWEGuNGSSENSOR, INPUT_PULLDOWN);
@@ -268,6 +396,9 @@ void setup() {
     mvE[i].ts = 0;
     mvE[i].state = 0;
   }
+  delay(1000);
+  connectToMqtt();
+  Serial.print("setup end......");
 }
 
 void loop() {
@@ -275,6 +406,9 @@ void loop() {
 
   // Look for and handle WebSocket data
   webSocket.loop();
+  if (ts > ts_DeepSleepTime) {
+    gotoDeepSleep();
+  }
   if (ts > ts_lastMovementRead + 2000) {
 
     ts_lastMovementRead = millis();
@@ -306,10 +440,11 @@ void loop() {
       serializeJson(jsondoc, msg_buf);
       // Serial.println(msg_buf);
       webSocket.sendTXT(0, msg_buf);
+      mqttClient.publish("keller/movement", 1, true, String(movement * 100).c_str());
     }
     moveState = movement;
   }
-  if (ts > ts_lastDistanceRead + 2000) {
+  if (ts > ts_lastDistanceRead + 10000) {
 
     uint distance = getDistance();
     ts_lastDistanceRead = millis();
@@ -321,10 +456,10 @@ void loop() {
     jsondoc["distance"] = distance;
     jsondoc["ts"] = ts_lastDistanceRead / 1000;
     serializeJson(jsondoc, msg_buf);
-
+    mqttClient.publish("keller/distance", 1, true, String(distance).c_str());
     webSocket.sendTXT(0, msg_buf);
   }
-  if (ts > ts_lastDHT11Read + 5000) {
+  if (ts > ts_lastDHT11Read + 30000) {
     ts_lastDHT11Read = millis();
     sensors_event_t event;
 
@@ -340,6 +475,7 @@ void loop() {
       Serial.print(F("Temperature: "));
       Serial.print(event.temperature);
       Serial.println(F("°C"));
+      mqttClient.publish("keller/temperatur", 1, true, String(event.temperature).c_str());
     }
     jsondoc["temperatur"] = event.temperature;
     // Get humidity event and print its value.
@@ -350,6 +486,7 @@ void loop() {
       Serial.print(F("Humidity: "));
       Serial.print(event.relative_humidity);
       Serial.println(F("%"));
+      mqttClient.publish("keller/humidity", 1, true, String(event.relative_humidity).c_str());
     }
     jsondoc["luftfeuchtigkeit"] = event.relative_humidity;
 
@@ -359,6 +496,92 @@ void loop() {
   }
 }
 
+void gotoDeepSleep() {
+  struct timeval now;
+  Serial.println("goto DeepSleep");
+  gettimeofday(&now, NULL);
+
+  Serial.printf("deep sleep (%lds since last reset, %lds since last boot)\n", now.tv_sec, now.tv_sec - last);
+
+  last = now.tv_sec;
+
+  esp_sleep_enable_timer_wakeup(1000000 * D_DeepSleepDauer); // set timer but don't sleep now
+                                                             // esp_bluedroid_disable, esp_bt_controller_disable, esp_wifi_stop
+  printf("config IO\n");
+  // Wake if low=0 high=1
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+  gpio_pulldown_en((gpio_num_t)D_BEWEGuNGSSENSOR);
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)D_BEWEGuNGSSENSOR, 1);
+
+  // pinMode(D_BEWEGuNGSSENSOR, INPUT_PULLDOWN);
+
+  printf("deep sleep\n");
+  esp_deep_sleep_start();
+  //!< is needed by one of the wakeup options.
+  //!< Otherwise power it down.
+  // gpio_set_direction(GPIO_INPUT_IO_TRIGGER, GPIO_MODE_INPUT);
+  // pinMode(GPIO_INPUT_IO_TRIGGER_DIO0, INPUT);
+
+  /*
+  However, if RTC peripherals are powered down, internal pullup and pulldown
+  resistors will be disabled. To use internal pullup or pulldown resistors,
+  request RTC peripherals power domain to be kept on during deep sleep, and
+  configure pullup/pulldown resistors using rtc_gpio_ functions, before
+  entering deep sleep:
+
+  esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+  gpio_pullup_dis(gpio_num);
+  gpio_pulldown_en(gpio_num);
+
+  Warning
+
+  After wake up from deep sleep, IO pad(s) used for wakeup will be
+  configured as RTC IO. Before using these pads as digital GPIOs,
+  reconfigure them using rtc_gpio_deinit(gpio_num) function.
+  */
+  /*
+    // Wake if low=0 high=1
+    // esp_sleep_enable_ext0_wakeup(GPIO_INPUT_IO_TRIGGER, 1);
+    // Only RTC IO can be used as a source for external wake
+    // source. They are pins: 0,2,4,12-15,25-27,32-39.
+    esp_sleep_enable_ext1_wakeup(BUTTON_WAKEUP_BITMASK, ESP_EXT1_WAKEUP_ANY_HIGH);
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_AUTO);
+    //!< Keep power domain enabled in deep sleep, if it
+    printf("forrrrrr\n");
+    for (uint8_t i = 0; i < 9; i++) {
+      delay(1);
+      Serial.print(".");
+    }
+    printf("deep sleep\n");
+    esp_deep_sleep_start();
+    */
+}
+
+esp_sleep_wakeup_cause_t print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch (wakeup_reason) {
+  case ESP_SLEEP_WAKEUP_EXT0:
+    Serial.println("Wakeup caused by external signal using RTC_IO");
+    break;
+  case ESP_SLEEP_WAKEUP_EXT1:
+    Serial.println("Wakeup caused by external signal using RTC_CNTL");
+    break;
+  case ESP_SLEEP_WAKEUP_TIMER:
+    Serial.println("Wakeup caused by timer");
+    break;
+  case ESP_SLEEP_WAKEUP_TOUCHPAD:
+    Serial.println("Wakeup caused by touchpad");
+    break;
+  case ESP_SLEEP_WAKEUP_ULP:
+    Serial.println("Wakeup caused by ULP program");
+    break;
+  default:
+    Serial.println("Wakeup was not caused by deep sleep: ");
+    break;
+  }
+  return wakeup_reason;
+}
 uint getDistance() {
   uint entfernung = 0;
   uint zeit = 0;
--- src/main.h
+++ src/main.h
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 
 #include <ESPAsyncWebServer.h>
+#include <esp_sleep.h>
 #include <SPIFFS.h>
 #include <WebSocketsServer.h>
 #include <WiFi.h>
@@ -16,17 +17,49 @@ extern "C" {
 #include "freertos/timers.h"
 }
 #include <AsyncMqttClient.h>
+#include <driver/rtc_io.h>
 
 #define D_SR04_EchoPin 14
 #define D_SR04_TriggerPin 12
 #define D_DHT11 13
 #define D_BEWEGuNGSSENSOR 15
 #define D_EVENTCOUNT 20
-
-int explode2int(String &s, char d);
-uint getDistance();
+#define D_DeepSleepDauer 90       // sec
+#define D_AwakeDauer 60           // sec
+#define D_DHT11ReadIntervall 10   // sec
+#define D_DistanceReadIntervall 3 // sec)
+#define D_MovementReadIntervall 2 // sec)
 
 typedef struct {
   uint8_t state;
   uint32_t ts;
 } moveEvent_t;
+
+#define MQTT_HOST IPAddress(192, 168, 18, 8)
+#define MQTT_PORT 1883
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+int explode2int(String &s, char d);
+uint getDistance();
+void connectToWifi();
+void connectToMqtt();
+
+void WiFiEvent(WiFiEvent_t event);
+
+void onMqttConnect(bool sessionPresent);
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason);
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos);
+
+void onMqttUnsubscribe(uint16_t packetId);
+
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+
+void onMqttPublish(uint16_t packetId);
+
+esp_sleep_wakeup_cause_t print_wakeup_reason();
+void gotoDeepSleep();
